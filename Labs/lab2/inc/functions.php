15f36f25559323006551d132a9ada930b782d2fa
Victoria Zamora <vzamora@csumb.edu>
2017-09-11 04:12:44 +0000

    adding lab2

--- Labs/lab2/inc/functions.php
+++ Labs/lab2/inc/functions.php
@@ -1,14 +1,57 @@
 <?php
-    function play(){
+    
         
-              
-        for($i=1; $i<4; $i++){
-            ${"randomVal" . $i} = rand(0,2);
-            displaySymbol(${"randomVal" . $i} );
-        }
-        displayPoints($randomVal1, $randomVal2, $randomVal3);
+          
+        function displaySymbol($randomValue, $pos){
+              $randomValue = rand(0,3);
+                    
+            switch($randomValue){
+            
+                case 0: $symbol = "seven";
+                    break;
+                case 1: $symbol = "cherry";
+                    break;
+                case 2: $symbol = "lemon";
+                    break;
+                case 3: $symbol = "grapes";
+            }    //end switch
+        
+        echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='" . ucfirst($symbol). "' width='70'>"  ;
+       }  
       
+    
+    
+          function displayPoints($randomValue1, $randomValue2, $randomValue3,$randomValue4){
+        
+            echo "<div id= 'output'>";
+            if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3){
+                switch($randomValue1){
+                    case 0: $totalPoints = 1000;
+                            echo "<h1> Jackpot! </h1>";
+                            break;
+                    case 1: $totalPoints = 900;
+                            break;
+                    case 2: $totalPoints = 500;
+                            break;
+                    case 3: $totalPoints = 250;
+                            break;
+            }
+            echo "<h2> You won $totalPoints points!</h2>";
+        } else{
+            echo "<h3> Try Again!  </h3>";
+        }
+        echo "</div>";
+        
+     
     }
+    
+       function play(){
+          for($i=0; $i<4; $i++){
+            ${"randomValue" . $i } = rand(0,3);
+           displaySymbol(${"randomValue" . $i}, $i );
+        }
+        displayPoints($randomValue1, $randomValue2, $randomValue3, $randomValue4);
+       }
 
 
 
--- Labs/lab2/index.php
+++ Labs/lab2/index.php
@@ -1,77 +1,30 @@
+<?php
+
+include 'inc/functions.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title> 777 Slot Machine </title>
         <meta charset="utf-8" />
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
         
+    <div id="main"
        <?php
-       
-            $randomVal1 = rand(0,2);
-            displaySymbol($randomVal1);
-     
-            $randomVal2 = rand(0,2);
-            displaySymbol($randomVal2);
-     
-            $randomVal3 = rand(0,2);
-            displaySymbol($randomVal3);
-        
-        function displaySymbol($randomVal1){
-              $randomVal = rand(0,2);
-                    
-            switch($randomVal){
-            
-                case 0: $symbol = "seven";
-                    break;
-                 case 1: $symbol = "grapes";
-                    break;
-                case 2: $symbol = "orange";
-                        break;
-            }    //end switch   
-        
-        echo "<img src='img/$symbol.png' alt='$symbol' title='" . ucfirst($symbol). "' width''70'>"  ;
-       }
-       
-        function displayPoints($randomVal1, $randomVal2, $randomVal3){
-        
-            echo "<div id= 'output'>";
-            if($randomVal1 == $randomVal2 && $randomVal2 == $randomVal3){
-                switch($randomVal1){
-                    case 0: $totalPoints = 1000;
-                            echo "<h1> Jackpot! </h1>";
-                            break;
-                    case 1: $totalPoints = 500;
-                            break;
-                    case 2: $totalPoints = 250;
-                            break;
-            }
-            echo "<h2> You won $totalPoints points!</h2>";
-        } else{
-            echo "<h3> Try Again!  </h3>";
-        }
-        echo "</div>";
-        
-    }
-
-      
-      
-      
-      
-      
-      
-      
-      
-        
-       // $symbol= temp;
-      /*
-        
-
-   
-           */
+  
+       play();
         ?>
-     
+     <form>
+         <input type="submit" value="Spin!"/>
+     </form>
         
+    </div>
 
     </body>
 </html>

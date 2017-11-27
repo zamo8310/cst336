f251d22779d39349527407b3c00b207d1264ee57
Victoria Zamora <vzamora@csumb.edu>
2017-11-27 02:23:02 +0000

    adding lab8

---
+++ Labs/lab8/checkUsername.php
@@ -0,0 +1,23 @@
+<?php
+
+
+include '../../dbConnection.php';
+$conn = getDatabaseConnection();
+
+
+$sql = "SELECT *
+        FROM tc_user
+        WHERE username = :username"; 
+
+$namedParameters = array();
+$namedParameters[':username'] = $_GET['username'];
+       
+        
+$stmt = $conn->prepare($sql);
+$stmt->execute($namedParameters);
+$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
+
+//print_r($record);
+
+echo json_encode($record);
+?>

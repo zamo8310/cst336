9976f8345fa55c546e26640e7a5eb95516f2a46d
Victoria Zamora <vzamora@csumb.edu>
2017-12-05 19:49:48 +0000

    fixing Lab10

---
+++ Labs/lab9/about.php
@@ -0,0 +1,22 @@
+<?php
+    include 'inc/header.php';
+?>
+            
+
+
+<div>
+    <p>
+        We are dedicated to finding animals in need happy homes!
+    </p>
+</div>
+
+
+
+
+
+
+
+
+<?php
+    include 'inc/footer.php';
+?>
